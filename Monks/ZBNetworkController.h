bffce50dc5b8a23684bca4203166b0ca285b8ea5
zuri1 <zuri.biringer@gmail.com>
2014-10-31 13:48:30 -0700

    Added More Talks table view controller

---
+++ Monks/ZBNetworkController.h
@@ -0,0 +1,13 @@
+//
+//  ZBNetworkController.h
+//  Monks
+//
+//  Created by Zuri Biringer on 7/2/14.
+//  Copyright (c) 2014 Zuri Biringer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZBNetworkController : NSObject
+
+@end
---
+++ Monks/ZBNetworkController.m
@@ -0,0 +1,13 @@
+//
+//  ZBNetworkController.m
+//  Monks
+//
+//  Created by Zuri Biringer on 7/2/14.
+//  Copyright (c) 2014 Zuri Biringer. All rights reserved.
+//
+
+#import "ZBNetworkController.h"
+
+@implementation ZBNetworkController
+
+@end
